92a32d3e53c640da06e52a9bcdd6a26e8b327e5c
Gautier Einsweiler <gautier.einsweiler@epitech.eu>
2025-10-06 13:06:29 -0400

    changing name

--- src/data/author.ts
+++ src/data/author.ts
@@ -1,9 +1,9 @@
 import avatar from '../assets/images/sample-avatar.jpg';
 
 export const author = {
-  name: 'Site Author',
+  name: 'Gautier Einsweiler',
   avatar,
-  url: '#',
+  url: 'https://media.licdn.com/dms/image/v2/D4E10AQEC3DReBSrxqA/image-shrink_800/image-shrink_800/0/1721195703351?e=2147483647&v=beta&t=nOejk6C6djARfAEe-NknFGfWXflbJHT-MJZPR_KovnA',
 };
 
 export type Author = typeof author;
