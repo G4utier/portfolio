0f0a66456c33d4450aa0044ff86f77a5367f0dca
Gautier Einsweiler <gautier.einsweiler@epitech.eu>
2025-10-06 14:57:00 -0400

    Adding name and personnal informations

--- src/data/author.ts
+++ src/data/author.ts
@@ -3,7 +3,7 @@ import avatar from '../assets/images/sample-avatar.jpg';
 export const author = {
   name: 'Gautier Einsweiler',
   avatar,
-  url: 'https://media.licdn.com/dms/image/v2/D4E10AQEC3DReBSrxqA/image-shrink_800/image-shrink_800/0/1721195703351?e=2147483647&v=beta&t=nOejk6C6djARfAEe-NknFGfWXflbJHT-MJZPR_KovnA',
+  url: '#',
 };
 
 export type Author = typeof author;
